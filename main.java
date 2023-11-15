e14925c749d85d7b53349f000e44fabfa7e9d40a
Richard Victor <richard.rosa@esolution.com.br>
2023-11-14 21:11:10 -0300

    Update main.java
    Corrigindo erro de falha na impressão da ultima parte do exercicio

--- main.java
+++ main.java
@@ -18,7 +18,6 @@ public class BinaryTreeOperations {
     public static void main(String[] args) {
         BinaryTreeOperations treeOperations = new BinaryTreeOperations();
 
-        // Sorteia 20 números de 0 a 100 e insere na árvore
         for (int i = 0; i < 20; i++) {
             int randomNumber = new Random().nextInt(101);
             treeOperations.insert(randomNumber);
@@ -30,7 +29,6 @@ public class BinaryTreeOperations {
         treeOperations.printPostOrder();
         treeOperations.printLevelOrder();
 
-        // Retira 5 elementos da árvore
         for (int i = 0; i < 5; i++) {
             int valueToRemove = treeOperations.getRandomValue();
             treeOperations.remove(valueToRemove);
@@ -76,17 +74,15 @@ public class BinaryTreeOperations {
         } else if (value > root.value) {
             root.right = removeRec(root.right, value);
         } else {
-            // Nó com apenas um filho ou nenhum filho
+
             if (root.left == null) {
                 return root.right;
             } else if (root.right == null) {
                 return root.left;
             }
 
-            // Nó com dois filhos: obter o sucessor in-order (menor nó à direita)
             root.value = minValue(root.right);
 
-            // Remover o sucessor in-order
             root.right = removeRec(root.right, root.value);
         }
 
@@ -174,10 +170,31 @@ public class BinaryTreeOperations {
 
     private int getRandomValue() {
         Random random = new Random();
-        int randomIndex = random.nextInt(20); // 20 é o número de elementos na árvore
-        int[] values = new int[20];
-        collectValues(root, values, 0);
-        return values[randomIndex];
+        int randomIndex = random.nextInt(countNodes(root));
+        return getNthValue(root, randomIndex);
+    }
+
+    private int getNthValue(TreeNode root, int n) {
+        if (root == null) {
+            return -1; // Valor sentinela para indicar que não há elementos suficientes
+        }
+
+        int leftCount = countNodes(root.left);
+
+        if (n < leftCount) {
+            return getNthValue(root.left, n);
+        } else if (n == leftCount) {
+            return root.value;
+        } else {
+            return getNthValue(root.right, n - leftCount - 1);
+        }
+    }
+
+    private int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
     }
 
     private void collectValues(TreeNode root, int[] values, int index) {
